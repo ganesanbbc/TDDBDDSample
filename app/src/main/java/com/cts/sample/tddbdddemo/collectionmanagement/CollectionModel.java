462d4ac683bdad61993ab0d179e5c24a2888947d
sagarwal2 <sourav.agarwal@cognizant.com>
2015-11-24 12:25:05 +0530

    Added Collection management structure

---
+++ app/src/main/java/com/cts/sample/tddbdddemo/collectionmanagement/CollectionModel.java
@@ -0,0 +1,17 @@
+package com.cts.sample.tddbdddemo.collectionmanagement;
+
+/**
+ * Created by 268526 on 11/23/2015.
+ */
+public class CollectionModel {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
